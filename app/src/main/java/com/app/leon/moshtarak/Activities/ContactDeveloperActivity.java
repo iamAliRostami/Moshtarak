4ee207e64a460eb0d6bd6fc62c33dc1d67ddead5
ali93angel <34213546+ali93angel@users.noreply.github.com>
2019-08-29 09:24:28 +0430

    new version

--- app/src/main/java/com/app/leon/moshtarak/Activities/ContactDeveloperActivity.java
+++ app/src/main/java/com/app/leon/moshtarak/Activities/ContactDeveloperActivity.java
@@ -1,16 +1,49 @@
 package com.app.leon.moshtarak.Activities;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.leon.moshtarak.R;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ContactDeveloperActivity extends AppCompatActivity {
+    @BindView(R.id.imageView1)
+    ImageView imageView1;
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
+    @BindView(R.id.textView1)
+    TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.contact_developer_activity);
+        ButterKnife.bind(this);
+
+//        Display display = getWindowManager().getDefaultDisplay();
+//        Point size = new Point();
+//        display.getSize(size);
+//        int width = size.x;
+        YoYo.with(Techniques.Tada)
+                .duration(1000)
+                .repeat(2)
+                .playOn(textView1);
+        imageView1.animate()
+                .scaleX(2f)
+                .scaleY(2f)
+                .translationYBy(170f)
+                .rotation(360)
+                .setDuration(2000);
     }
 }
