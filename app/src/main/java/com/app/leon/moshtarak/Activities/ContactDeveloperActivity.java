8270016281f342b87fc8754076f5f5690e59f0d5
ali93angel <34213546+ali93angel@users.noreply.github.com>
2019-08-31 09:42:51 +0430

    new version

--- app/src/main/java/com/app/leon/moshtarak/Activities/ContactDeveloperActivity.java
+++ app/src/main/java/com/app/leon/moshtarak/Activities/ContactDeveloperActivity.java
@@ -1,14 +1,20 @@
 package com.app.leon.moshtarak.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.leon.moshtarak.R;
+import com.app.leon.moshtarak.Utils.FontManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +26,22 @@ public class ContactDeveloperActivity extends AppCompatActivity {
     ImageView imageView2;
     @BindView(R.id.textView1)
     TextView textView1;
+    @BindView(R.id.linearLayout1)
+    LinearLayout linearLayout1;
+    @BindView(R.id.linearLayout2)
+    LinearLayout linearLayout2;
+    @BindView(R.id.relativeLayout)
+    RelativeLayout relativeLayout;
+    Context context;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("message/rfc822");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ali.rostami33@gmail.com", "infoshamsaii@gmail.com", "mantera.sh@gmail.com"});
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +50,11 @@ public class ContactDeveloperActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.contact_developer_activity);
         ButterKnife.bind(this);
-
+        context = this;
+        FontManager fontManager = new FontManager(context);
+        fontManager.setFont(relativeLayout);
+        linearLayout1.setOnClickListener(onClickListener);
+        linearLayout2.setOnClickListener(onClickListener);
 //        Display display = getWindowManager().getDefaultDisplay();
 //        Point size = new Point();
 //        display.getSize(size);
