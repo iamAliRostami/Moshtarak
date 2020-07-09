3bf7e7510be155dd8ff79c9866218a156cf3aaaa
ali_angel <ali.rostami33@gmail.com>
2020-07-09 19:00:08 +0430

    new version

--- app/src/main/java/com/app/leon/moshtarak/Activities/HelpActivity.java
+++ app/src/main/java/com/app/leon/moshtarak/Activities/HelpActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Debug;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -20,6 +21,10 @@ import com.app.leon.moshtarak.R;
 import com.app.leon.moshtarak.Utils.SharedPreference;
 import com.app.leon.moshtarak.databinding.HelpContentBinding;
 
+import java.io.File;
+import java.util.Date;
+import java.util.Objects;
+
 public class HelpActivity extends BaseActivity //implements OnPageChangeListener, OnLoadCompleteListener {
 {
 //    public static final String SAMPLE_FILE = "1.pdf";
@@ -35,37 +40,26 @@ public class HelpActivity extends BaseActivity //implements OnPageChangeListener
     SharedPreference sharedPreference;
     Context context;
 
-    @SuppressLint({"SetJavaScriptEnabled", "CutPasteId"})
-    @Override
-    protected void initialize() {
-        binding = HelpContentBinding.inflate(getLayoutInflater());
-        View childLayout = binding.getRoot();
-        ConstraintLayout parentLayout = findViewById(R.id.base_Content);
-        parentLayout.addView(childLayout);
-        context = this;
-        sharedPreference = new SharedPreference(context);
-
-        progressDialog = new ProgressDialog(HelpActivity.this);
-        progressDialog.setMessage(getString(R.string.waiting));
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-        binding.webViewHelp.setWebChromeClient(new HelpWebChromeClient());
-
-        binding.webViewHelp.getSettings().setJavaScriptEnabled(true);
-        binding.webViewHelp.getSettings().setBuiltInZoomControls(true);
-
-        if (sharedPreference.getCache()) {
-            Log.e("cache", "exist!");
-            binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
-        } else {
-            Log.e("cache", "not exist!");
-            binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+    static int clearCacheFolder(final File dir, final int numDays) {
+        int deletedFiles = 0;
+        if (dir != null && dir.isDirectory()) {
+            try {
+                for (File child : Objects.requireNonNull(dir.listFiles())) {
+                    //first delete subdirectories recursively
+                    if (child.isDirectory()) {
+                        deletedFiles += clearCacheFolder(child, numDays);
+                    }
+                    if (child.lastModified() < new Date().getTime() - numDays * DateUtils.DAY_IN_MILLIS) {
+                        if (child.delete()) {
+                            deletedFiles++;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TAG", String.format("Failed to clean the cache, error %s", e.getMessage()));
+            }
         }
-        binding.webViewHelp.setWebViewClient(new HelpWebViewClient());
-        binding.webViewHelp.loadUrl(getString(R.string.abfa_site));
-//        binding.webViewHelp.loadUrl("www.google.com");
-//        binding.webViewHelp.loadUrl(URL);
-
+        return deletedFiles;
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -84,26 +78,40 @@ public class HelpActivity extends BaseActivity //implements OnPageChangeListener
         super.onBackPressed();
     }
 
-    private class HelpWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+    @SuppressLint({"SetJavaScriptEnabled", "CutPasteId"})
+    @Override
+    protected void initialize() {
+        binding = HelpContentBinding.inflate(getLayoutInflater());
+        View childLayout = binding.getRoot();
+        ConstraintLayout parentLayout = findViewById(R.id.base_Content);
+        parentLayout.addView(childLayout);
+        context = this;
+        clearCacheFolder(context.getCacheDir(), 1);
+        sharedPreference = new SharedPreference(context);
 
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            sharedPreference.putCache(true);
-            if (progressDialog.isShowing()) {
-                progressDialog.dismiss();
-            }
-        }
+        progressDialog = new ProgressDialog(HelpActivity.this);
+        progressDialog.setMessage(getString(R.string.waiting));
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        binding.webViewHelp.setWebChromeClient(new HelpWebChromeClient());
 
-        @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            Toast.makeText(HelpActivity.this, getString(R.string.error).concat(" : ")
-                    .concat(getString(R.string.error_IO)), Toast.LENGTH_SHORT).show();
-        }
+        binding.webViewHelp.getSettings().setJavaScriptEnabled(true);
+        binding.webViewHelp.getSettings().setBuiltInZoomControls(true);
+        binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        if (sharedPreference.getCache()) {
+//            Log.e("cache", "exist!");
+//            binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+////            binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        } else {
+//            Log.e("cache", "not exist!");
+//            binding.webViewHelp.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+//        }
+        binding.webViewHelp.setWebViewClient(new HelpWebViewClient());
+        binding.webViewHelp.loadUrl(getString(R.string.abfa_site));
+
+//        binding.webViewHelp.loadUrl("www.google.com");
+//        binding.webViewHelp.loadUrl(URL);
+
     }
 
     private class HelpWebChromeClient extends WebChromeClient {
@@ -118,6 +126,28 @@ public class HelpActivity extends BaseActivity //implements OnPageChangeListener
             }
             if (progress == 100)
                 setTitle(R.string.help);
+        }
+    }
+
+    private class HelpWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+//            sharedPreference.putCache(true);
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(HelpActivity.this, getString(R.string.error).concat(" : ")
+                    .concat(getString(R.string.error_IO)), Toast.LENGTH_SHORT).show();
         }
     }
 
